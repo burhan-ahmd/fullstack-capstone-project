f9e0f1224d4f6a8fdeeeaf3b1937f75a0ab0d79e
Burhan Ahmed <burrnn007@gmail.com>
2026-09-17 12:57:15 +0500

    Update MongoDB URI to use environment variable

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb');
 
-const uri = process.env.MONGODB_URI || 'mongodb+srv://burrnn007_db_user:TcMPrgiSlN4IQEgZ@cluster.mongodb.net/giftlink';
+const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
 let db;
